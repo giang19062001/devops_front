1368a5ece57f72309f9e0cc87dc24c8f6d5bae4d
NGO HOANG GIANG <meobapgiang19062001@gmail.com>
2025-05-07 13:29:16 +0700

    push image

--- src/App.js
+++ src/App.js
@@ -2,25 +2,28 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-    const [data, setData] = useState([]);
+   const [data, setData] = useState([]);
 
-    useEffect(() => {
-        axios
-            .get(`${process.env.REACT_APP_BACKEND}/products`)
-            .then((response) => setData(response.data))
-            .catch((error) => console.error(error));
-    }, []);
+   useEffect(() => {
+      axios
+         .get(`${process.env.REACT_APP_BACKEND}/products`)
+         .then((response) => setData(response.data))
+         .catch((error) => console.error(error));
+   }, []);
 
-    return (
-        <div>
-            <h1>Product List</h1>
-            <ul>
-                {data.map((product) => (
-                    <li key={product.id}>{product.name}</li>
-                ))}
-            </ul>
-        </div>
-    );
+   return (
+      <div>
+         <h1>Product List</h1>
+         <ul style={{display:"flex", flexDirection:"column", gap:5}}>
+            {data.map((product) => (
+               <li key={product.id} style={{ display: "flex", alignItems: "center", gap: 10 }}>
+                  <img src={`${process.env.REACT_APP_BACKEND}/${product.image}`} alt="" style={{ width: 150 }} />
+                  <b>{product.name}</b>
+               </li>
+            ))}
+         </ul>
+      </div>
+   );
 }
 
 export default App;
