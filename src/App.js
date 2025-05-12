1af0244784e54da90d8228ef7af414b3f66279e4
NGO HOANG GIANG <meobapgiang19062001@gmail.com>
2025-05-12 11:03:43 +0700

    test jenskin

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
    }, []);
    return (
       <div>
-         <h1>User: <i style={{color:"green"}}>{user?.username}</i></h1>
+         <h1>User: <i style={{color:"red"}}>{user?.username}</i></h1>
          <h1>Product List</h1>
          <ul style={{display:"flex", flexDirection:"column", gap:5}}>
             {data.map((product) => (
