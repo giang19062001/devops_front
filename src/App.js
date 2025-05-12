e4d55962c88d892225b04098c8b7699864d3794b
NGO HOANG GIANG <meobapgiang19062001@gmail.com>
2025-05-12 13:46:41 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Blog from "./components/Blog";
+import Home from "./components/Home";
+import Layout from "./components/Layout";
 
 function App() {
-   const [data, setData] = useState([]);
-   const [user, setUser] = useState("");
-
-   useEffect(() => {
-      axios
-         .get(`${process.env.REACT_APP_BACKEND}/products`)
-         .then((response) => setData(response.data))
-         .catch((error) => console.error(error));
-   }, []);
- useEffect(() => {
-      axios
-         .get(`${process.env.REACT_APP_BACKEND}/user`)
-         .then((response) => setUser(response.data))
-         .catch((error) => console.error(error));
-   }, []);
    return (
-      <div>
-         <h1>User: <i style={{color:"green"}}>{user?.username}</i></h1>
-         <h1>Product List</h1>
-         <ul style={{display:"flex", flexDirection:"column", gap:5}}>
-            {data.map((product) => (
-               <li key={product.id} style={{ display: "flex", alignItems: "center", gap: 10 }}>
-                  <img src={`${process.env.REACT_APP_BACKEND}/${product.image}`} alt="" style={{ width: 150 }} />
-                  <b>{product.name}</b>
-               </li>
-            ))}
-         </ul>
-      </div>
+      <BrowserRouter>
+         <Routes>
+            <Route path="/" element={<Layout></Layout>}>
+               <Route index element={<Home />} />
+               <Route path="blog" element={<Blog />} />
+            </Route>
+         </Routes>
+      </BrowserRouter>
    );
 }
 
